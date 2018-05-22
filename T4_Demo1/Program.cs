6805eff8bdacf55a9583b000ae1f50ab4f11f974
tww24098 <tww24098@TWW24098>
2018-05-22 15:48:38 +0800

    TimeSpan1

---
+++ T4_Demo1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4_Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
--- TimeSpan/Program.cs
+++ TimeSpan/Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace test
+namespace TimeSpan1
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             DateTime d1 = DateTime.Now;
@@ -18,11 +16,6 @@ namespace test
             TimeSpan ts = d2 - d1;
             var s = ts.ToString(@"dd\:hh\:mm\:ss");
             var s1 = TimeSpan.FromMilliseconds(ts.TotalMilliseconds).ToString(@"dd\:hh\:mm\:ss");
-        }
-
-        public static void ReceiveDelegateArgsFunc(Func<int, string> func)
-        {
-            Console.WriteLine(func(21));
         }
     }
 }
