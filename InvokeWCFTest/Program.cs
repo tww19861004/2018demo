49b29c24449e9178a73ec01ce12ce998270d26db
tww24098 <tww24098@TWW24098>
2018-08-28 22:24:20 +0800

    12341234

---
+++ InvokeWCFTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using InvokeWCFTest.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvokeWCFTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "1111";
+            
+            var proxy = WcfInvokeFactory.CreateServiceByUrl<ITemplateWebservice>(url);
+            Response response = proxy.GetTemplate("12011", null);
+                
+                int result = 1;
+        }
+    }
+}
