a2ce45a6fd5333fd0d94524fdc9c60105d681341
唐伟伟 <tww24098@ly.com>
2018-08-30 16:47:29 +0800

    1

--- InvokeWCFTest/Program.cs
+++ InvokeWCFTest/Program.cs
@@ -1,5 +1,4 @@
-﻿using InvokeWCFTest.ServiceReference1;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -16,45 +15,45 @@ namespace InvokeWCFTest
         static void Main(string[] args)
         {
 
-            string url = "xxx.svc";
-            var proxy = WcfInvokeFactory.CreateServiceByUrl<InvokeWCFTest.ServiceReference1.ITemplateWebservice>(url);
-            var result = proxy.GetTemplateInfo(new GetTemplateInfoRequest() { templateNo = "12011" });
+            //string url = "xxx.svc";
+            //var proxy = WcfInvokeFactory.CreateServiceByUrl<InvokeWCFTest.ServiceReference1.ITemplateWebservice>(url);
+            //var result = proxy.GetTemplateInfo(new GetTemplateInfoRequest() { templateNo = "12011" });
 
-            return;
+            //return;
 
-            TemplateWebserviceClient client = null;
-            TemplateInfo template1 = null;
-            try
-            {
-                client = new TemplateWebserviceClient();
-                template1 = new TemplateInfo();
-                var res1 = client.GetTemplate("12219", BuildTemplateTagDataCollection(new Dictionary<string, string>() {
-                { "checkInDate","2018-09-01 00:00:00.0"}}));
-                //var res2 = client.GetTemplateInfo("12011", out template1);
+            //TemplateWebserviceClient client = null;
+            //TemplateInfo template1 = null;
+            //try
+            //{
+            //    client = new TemplateWebserviceClient();
+            //    template1 = new TemplateInfo();
+            //    var res1 = client.GetTemplate("12219", BuildTemplateTagDataCollection(new Dictionary<string, string>() {
+            //    { "checkInDate","2018-09-01 00:00:00.0"}}));
+            //    //var res2 = client.GetTemplateInfo("12011", out template1);
 
-                if (template1!=null)
-                {
-                    if(template1.TemplateTagsk__BackingField!=null 
-                        && template1.TemplateTagsk__BackingField.Any())
-                    {
-                        foreach (var item in template1.TemplateTagsk__BackingField)
-                        {
+            //    if (template1!=null)
+            //    {
+            //        if(template1.TemplateTagsk__BackingField!=null 
+            //            && template1.TemplateTagsk__BackingField.Any())
+            //        {
+            //            foreach (var item in template1.TemplateTagsk__BackingField)
+            //            {
 
-                        }
-                    }
-                }
-            }
-            catch(Exception ex)
-            {
+            //            }
+            //        }
+            //    }
+            //}
+            //catch(Exception ex)
+            //{
 
-            }
-            finally
-            {
-                if(client!=null)
-                {
-                    client.Close();
-                }
-            }
+            //}
+            //finally
+            //{
+            //    if(client!=null)
+            //    {
+            //        client.Close();
+            //    }
+            //}
                                     
             //string url = "http://hotelapi.vip.elong.com/TemplateService/TemplateService.svc";
             //GetTemplateInfoRequest request = new GetTemplateInfoRequest() { templateNo = "12011" };
@@ -70,62 +69,5 @@ namespace InvokeWCFTest
             //}
         }
 
-        /// <summary>
-        /// 获取模板对象
-        /// </summary>
-        /// <param name="templateParams"></param>
-        /// <returns></returns>
-        public static TemplateTagDataCollection BuildTemplateTagDataCollection(
-                Dictionary<String, String> templateParams)
-        {
-            // 模板的业务对象
-            TemplateTagDataCollection tagDataCollection = new TemplateTagDataCollection();
-            tagDataCollection.Dictionary = new Dictionary<string, TemplateTagData>();
-
-            foreach (var entry in templateParams)
-            {
-                if (entry.Value == null)
-                {
-                    continue;
-                }
-                if (entry.Value.StartsWith("LISTS:"))
-                {
-                    // 判断入参是list,list里边是一个map
-
-                    // 模板数据集合
-                    TemplateTagData valueDate = new TemplateTagData();
-                    List<TemplateTagDataCollection> childTags = new List<TemplateTagDataCollection>();
-                    string json = (entry.Value).Trim().Substring(
-                                    "LISTS:".Length,
-                                    entry.Value.Length);
-                    List<Dictionary<string, string>> paramsList = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(json);
-
-                    // 设置集合的lists
-                    valueDate.TagKey = entry.Key.Trim();
-
-                    foreach (var paraMaps in paramsList)
-                    {
-                        childTags.Add(BuildTemplateTagDataCollection(paraMaps));
-
-                    }
-                    valueDate.ChildTag = childTags.ToArray();
-                    tagDataCollection.Dictionary.Add(entry.Key.Trim(), valueDate);
-
-                }
-                else
-                {
-                    // 模板内容对象
-
-                    TemplateTagData valueDate = new TemplateTagData();
-                    valueDate.TagValue = entry.Value != null ? entry.Value.Trim() : null;
-                    valueDate.TagKey = (null);// 默认为null
-                    valueDate.ChildTag = (null);// 默认为null
-
-                    tagDataCollection.Dictionary.Add(entry.Key.Trim(), valueDate);
-
-                }
-            }
-            return tagDataCollection;
-        }
     }
 }
---
+++ httpclientdemo1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace httpclientdemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = get().Result;
+            Console.WriteLine("test12345");
+        }
+
+        public async static Task<string> get()
+        {
+            string result = string.Empty;
+            HttpClient hc = new HttpClient();
+            //HttpContent contentPost = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await hc.GetAsync("https://www.baidu.com/");
+            try
+            {
+                response.EnsureSuccessStatusCode();
+                result = await response.Content.ReadAsStringAsync();
+                // Handle success
+            }
+            catch (HttpRequestException ex)
+            {
+                result = ex.Message;
+                // Handle failure
+            }
+            return result;
+        }
+    }
+}
