1ab31bddf8702dbad3b4f5a0110b86c07450908c
tww24098 <tww24098@TWW24098>
2018-09-01 15:39:30 +0800

    3

---
+++ reflection_demo/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reflection_demo
+{
+    public class EventTypeAttribute : Attribute
+    {
+        private int index;
+        private String name;
+        private String desc;
+        private String templateNo;
+        private String type;
+        public EventTypeAttribute(int index, string name, string desc, string templateNo, string type)
+        {
+            this.index = index;
+            this.name = name;
+            this.desc = desc;
+            this.templateNo = templateNo;
+            this.type = type;
+        }
+    }
+    public class EventType
+    {
+        /// <summary>
+        /// 缓存数据
+        /// </summary>
+        public static readonly List<EventTypeAttribute> values;
+        static EventType()
+        {
+            values = InitData();
+        }
+        public static List<EventTypeAttribute> InitData()
+        {
+            List<EventTypeAttribute> res = null;
+            Type t = typeof(EventType);
+            FieldInfo[] fieldInfoList = t.GetFields();
+            if (fieldInfoList != null && fieldInfoList.Any())
+            {
+                res = new List<EventTypeAttribute>();
+                EventTypeAttribute temp = null;
+                foreach (FieldInfo fieldInfo in fieldInfoList)
+                {
+                    temp = fieldInfo.GetValue(t) as EventTypeAttribute;
+                    if (temp!=null)
+                    {
+                        res.Add(temp);
+                    }                    
+                }
+            }
+            return res;
+        }
+        /// <summary>
+        /// 支付失败,sms
+        /// </summary>
+        public static EventTypeAttribute SMS_TEMPLATE_PAY_FAIL = new EventTypeAttribute(-1, "payFailure", "支付失败", "1234", "sms");
+
+        /// <summary>
+        /// 支付成功,sms
+        /// </summary>
+        public static EventTypeAttribute SMS_TEMPLATE_PAY_SUCCESS = new EventTypeAttribute(1, "paySuccess", "支付成功", "5678", "sms");
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = EventType.values;
+        }
+    }
+}
